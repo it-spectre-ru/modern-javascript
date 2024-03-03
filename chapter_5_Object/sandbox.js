df5a0cdc140a347e80fcaf6843389821d6e5885a
Note <it.spectre.ru@gmail.com>
2024-03-03 10:43:08 +0300

    creating an object

---
+++ chapter_5_Object/sandbox.js
@@ -0,0 +1,22 @@
+
+let user = {
+  name: 'Mike',  age: 33,
+  email: 'free@user.com',
+  location: 'Town',
+  blog: ['Chapter1', '222222']
+}
+
+console.log(user)
+console.log(user.name)
+
+// user.age = 11
+
+console.log(user.age)
+
+
+console.log(user['name'])
+
+console.log(typeof user)
+
+
+
