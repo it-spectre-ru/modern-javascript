753ea3498e050134bd192373fa74f635dda1426b
Note <it.spectre.ru@gmail.com>
2024-06-25 08:47:44 +0300

    the query selector

---
+++ chapter_6_Dom/sandbox.js
@@ -0,0 +1,31 @@
+const para = document.querySelector('p');
+const sel = document.querySelector('.error');
+const div = document.querySelector('div.error');
+const bod = document.querySelector('body > h1');
+
+console.log(bod)
+
+
+console.log('---------------------')
+console.log('---------------------')
+console.log('---------------------')
+
+
+
+const paras = document.querySelectorAll('p')
+
+console.log(paras)
+console.log(paras[0])
+
+console.log('---------------------')
+console.log('---------------------')
+console.log('---------------------')
+
+paras.forEach(para => {
+  console.log(para)
+})
+
+
+const errors = document.querySelectorAll('.error')
+
+console.log(errors)
